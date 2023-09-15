c445e5a8d84fc5a01f6549cc43e5e59c9c26e3bc
Tomas-Falcon <tomasfalconblog@gmail.com>
2023-09-15 19:08:17 +0200

    V2

--- EX00 MultiPadre (dos interface y la que ereda de ambas)/src/ClaseMainControlInterface.java
+++ EX00 MultiPadre (dos interface y la que ereda de ambas)/src/ClaseMainControlInterface.java
@@ -10,26 +10,27 @@ public class ClaseMainControlInterface  implements Interface1, Interface2 {
 		Interface3.metodo1();
 		Interface2.metodo4();
 		
-		//Preguntarle a ALEX
-		
+	}
+	
+	public void metoto1() {
 		metodo1();
 		metodo2();
 		metodo3();
-		//Preguntarle a ALEX
 	}
+	
 	@Override
 	public void metodo1() {
-		System.out.print("metodo 1 re escrito que viene de la interface 1 o 2");
+		System.out.println("metodo 1 re escrito que viene de la interface 1 o 2");
 	}
 
 	@Override
 	public void metodo2() {
-		System.out.print("metodo 2 re escrito que viene de la interface 1 o 2");
+		System.out.println("metodo 2 re escrito que viene de la interface 1 o 2");
 	}
 
 	@Override
 	public void metodo3() {
-		System.out.print("metodo 3 re escrito que viene de la interface 1 o 2");		
+		System.out.println("metodo 3 re escrito que viene de la interface 1 o 2");		
 	}
 	
 	
