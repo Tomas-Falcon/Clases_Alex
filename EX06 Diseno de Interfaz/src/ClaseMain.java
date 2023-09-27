38de0d014e920238ae3bd2f867819eefca8088e5
Tomas-Falcon <tomasfalconblog@gmail.com>
2023-09-27 21:20:25 +0200

    V5

--- EX06 Diseno de Interfaz/src/ClaseMain.java
+++ EX06 Diseno de Interfaz/src/ClaseMain.java
@@ -6,7 +6,7 @@ public class ClaseMain {
 		f.setSize(400, 300);
 		f.setTitle("Hola awt");
 		
-		Button boton = new Button(); 
+		Button boton = new Button("Nombre"); 
 		boton.setSize(200, 40);
 		boton.setLocation(100, 100);
 		f.setLayout(null);
