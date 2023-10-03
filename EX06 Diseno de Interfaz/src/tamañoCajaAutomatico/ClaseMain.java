ac5ccc2b65ae8af6c1a056c52cb024bd90d858a3
Tomas-Falcon <tomasfalconblog@gmail.com>
2023-10-03 17:28:54 +0200

    V4

---
+++ EX06 Diseno de Interfaz/src/tamañoCajaAutomatico/ClaseMain.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package tamañoCajaAutomatico;
+
+import java.awt.*;
+import java.util.ArrayList;
+public class ClaseMain {
+
+	public static void main(String[] args) {
+		Frame f = new Frame();
+		f.setSize(500, 400);
+		f.setTitle("Hola awt");
+		
+		ArrayList<String>  arraylistCajadeTexto = new ArrayList<>(5);
+		
+		arraylistCajadeTexto.add("Nombre");
+		arraylistCajadeTexto.add("Apellido");
+		arraylistCajadeTexto.add("Edad");
+		arraylistCajadeTexto.add("Numero de telefono");
+		
+		TextArea cajaTexto1 = new TextArea(arraylistCajadeTexto.get(0));
+		TextArea cajaTexto2 = new TextArea(arraylistCajadeTexto.get(1));
+		TextArea cajaTexto3 = new TextArea(arraylistCajadeTexto.get(2));
+		TextArea cajaTexto4 = new TextArea(arraylistCajadeTexto.get(3));
+		
+		int tamañoTotal = 0;
+		
+		for (String comparar : arraylistCajadeTexto) {
+			if(tamañoTotal < comparar.length()) {
+				tamañoTotal = comparar.length();
+			}
+		}
+		TextField escribirTexto1 = new TextField();
+		TextField escribirTexto2 = new TextField();
+		TextField escribirTexto3 = new TextField();
+		TextField escribirTexto4 = new TextField();
+		
+		tamañoTotal = tamañoTotal * 8;
+		//boton.setBounds(x, y, ancho, alto);
+		int x = 50;
+		cajaTexto1.setBounds(x, 50, tamañoTotal, 40);
+		cajaTexto2.setBounds(x, 110, tamañoTotal, 40);
+		cajaTexto3.setBounds(x, 200, tamañoTotal, 40);
+		cajaTexto4.setBounds(x, 290, tamañoTotal, 40);
+		int x2 = (x*2) + tamañoTotal;
+		escribirTexto1.setBounds(x2, 50, tamañoTotal, 40);
+		escribirTexto2.setBounds(x2, 110, tamañoTotal, 40);
+		escribirTexto3.setBounds(x2, 200, tamañoTotal, 40);
+		escribirTexto4.setBounds(x2, 290, tamañoTotal, 40);
+		
+		
+		f.setLayout(null);
+		
+		f.add(cajaTexto1);
+		f.add(cajaTexto2);
+		f.add(cajaTexto3);
+		f.add(cajaTexto4);
+		
+		f.add(escribirTexto1);
+		f.add(escribirTexto2);
+		f.add(escribirTexto3);
+		f.add(escribirTexto4);
+		
+		f.setBackground(Color.gray);
+		
+		f.setVisible(true);
+		
+	}
+
+}
