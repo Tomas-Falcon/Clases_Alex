373882a99346c0601b26b2b54a8a20579b3c7212
Tomas-Falcon <tomasfalconblog@gmail.com>
2023-10-05 21:24:30 +0200

    V1

---
+++ EX07 Conexxion a base de datos/src/app/ClaseMain.java
@@ -0,0 +1,28 @@
+package app;
+import java.sql.*;
+
+/**
+ * 
+ */
+public class ClaseMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+			Class.forName("com.mysql.jdbc.Drive");
+			String url = "";
+			String usuario = "root";
+			String contraseña = "mysql";
+			
+			Conection conn = DriverManager.getConnection(url,usuario,contraseña);
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
