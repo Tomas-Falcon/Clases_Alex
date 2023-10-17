bfc61d0b0e46f795a416e4e1382ff96b5cc15500
Tomas-Falcon <tomasfalconblog@gmail.com>
2023-10-17 17:19:30 +0200

    Actualizacion automatica

--- EX06 Diseno de Interfaz/src/escuchame/ClaseEscuchame.java
+++ EX06 Diseno de Interfaz/src/escuchame/ClaseEscuchame.java
@@ -30,18 +30,56 @@ public class ClaseEscuchame extends Frame implements ActionListener{
 		fl.setAlignment(FlowLayout.LEFT);
 		setLayout(fl);
 		
+		
 		Button b1 = new Button("Bontoncin");
+		b1.setActionCommand("i1");
 		b1.addActionListener(this);
 		add(b1);
+		
+		Button b2 = new Button("Bontoncito");
+		b2.setActionCommand("i2");
+		b2.addActionListener(this);
+		add(b2);
+		
+		Button b3 = new Button("Bontoncin");
+		b3.setActionCommand("i3");
+		b3.addActionListener(this);
+		add(b3);
 		
 		setVisible(true);
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		System.out.println("botoncin precionado");
+		String etiqueta = e.getActionCommand();
+		/*
+			var tiqueta = e.getSource(); 
+			System.out.println(tiqueta);
+			String etiqueta2 = tiqueta.toString();
+			//16 23
+			int a = 16, b = 23;
+			String etiqueta3 = etiqueta2.substring(a,b);
+		*/
+		
+		
+			switch (etiqueta) {
+			case "i1" : 
+				System.out.println("Hola Bontoncin");
+				break;
+			case "i2" :
+				System.out.println("Adios Bontoncito");
+				break;
+				
+			case "i3" :
+				System.out.println("no soy Bontoncin");
+				break;
+			}
+		
 	}
 	
 	
-
+	
+	
 }
+
+
