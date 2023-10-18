c19a9a946ff4c26f49c0a47a457e309f00d180c6
Tomas-Falcon <tomasfalconblog@gmail.com>
2023-10-18 21:22:32 +0200

    Actualizacion automatica

--- EX06 Diseno de Interfaz/src/escuchame/ClaseEscuchame.java
+++ EX06 Diseno de Interfaz/src/escuchame/ClaseEscuchame.java
@@ -21,6 +21,11 @@ public class ClaseEscuchame extends Frame implements ActionListener{
 	public static void main(String[] args) {
 		ClaseEscuchame claseEscuchame = new ClaseEscuchame();
 	}
+	
+	Button b1 = new Button("Bontoncin");
+	Button b2 = new Button("Bontoncito");
+	Button b3 = new Button("Bontoncin");
+	
 	public ClaseEscuchame() {
 		
 		super("Escuchame");
@@ -31,17 +36,17 @@ public class ClaseEscuchame extends Frame implements ActionListener{
 		setLayout(fl);
 		
 		
-		Button b1 = new Button("Bontoncin");
+	
 		b1.setActionCommand("i1");
 		b1.addActionListener(this);
 		add(b1);
 		
-		Button b2 = new Button("Bontoncito");
+		
 		b2.setActionCommand("i2");
 		b2.addActionListener(this);
 		add(b2);
 		
-		Button b3 = new Button("Bontoncin");
+		
 		b3.setActionCommand("i3");
 		b3.addActionListener(this);
 		add(b3);
@@ -51,35 +56,39 @@ public class ClaseEscuchame extends Frame implements ActionListener{
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		String etiqueta = e.getActionCommand();
+		Button boton = (Button)e.getSource();
+		if(boton==b1) {
+			System.out.println("Hola Bontoncin    (i1 || button0 || b1)");
+			b1.setBackground(Color.getHSBColor(118, 100, 69));
+			
+		}else if(boton==b2) {
+			System.out.println("Adios Bontoncito    (i2 || button1 || b2)");
+			b2.setBackground(Color.gray);
+		}else if(boton==b3) {
+			System.out.println("no soy Bontoncin    (i3 || button2 || b3)");
+			b3.setBackground(Color.getHSBColor(14, 96, 100));
+		}
+		
+		
+		
 		/*
-			var tiqueta = e.getSource(); 
-			System.out.println(tiqueta);
-			String etiqueta2 = tiqueta.toString();
-			//16 23
-			int a = 16, b = 23;
-			String etiqueta3 = etiqueta2.substring(a,b);
-		*/
-		
-		
-			switch (etiqueta) {
-			case "i1" : 
-				System.out.println("Hola Bontoncin");
+		  var etiqueta2 = e.getSource();
+		String etiqueta3 = etiqueta2.toString();
+		int a = 16, b = 23;
+		etiqueta3 = etiqueta3.substring(a,b);
+		System.out.println(etiqueta3);	
+			switch (etiqueta3) {
+			case "button0" : 
+				System.out.println("Hola Bontoncin    (i1 || button0 || b1)");
 				break;
-			case "i2" :
-				System.out.println("Adios Bontoncito");
+			case "button1" :
+				System.out.println("Adios Bontoncito    (i2 || button1 || b2)");
 				break;
-				
-			case "i3" :
-				System.out.println("no soy Bontoncin");
+			case "button2" :
+				System.out.println("no soy Bontoncin    (i3 || button2 || b3)");
 				break;
-			}
+			}*/
 		
 	}
 	
-	
-	
-	
 }
-
-
