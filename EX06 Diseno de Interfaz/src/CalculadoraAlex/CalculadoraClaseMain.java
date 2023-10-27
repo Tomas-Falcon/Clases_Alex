1e560322af16db3fd0a2adb3944c2dee7567abe8
Tomas-Falcon <tomasfalconblog@gmail.com>
2023-10-27 19:47:25 +0200

    Actualizacion automatica

--- EX06 Diseno de Interfaz/src/CalculadoraAlex/CalculadoraClaseMain.java
+++ EX06 Diseno de Interfaz/src/CalculadoraAlex/CalculadoraClaseMain.java
@@ -55,41 +55,60 @@ public class CalculadoraClaseMain extends Frame {
         centroPanel.setLayout(new GridLayout(4, 3));
         
         Button nueve = new Button("9");
-        nueve.addActionListener(new ActionListener() {public void actionPerformed(ActionEvent e) {panelDeLosNumeros = panelDeLosNumeros + "9";tf.setText(panelDeLosNumeros);}});
+        nueve.addActionListener(new ActionListener() {public void actionPerformed(ActionEvent e) {if(simbolo.equalsIgnoreCase("ñ")) {panelDeLosNumeros = panelDeLosNumeros + "9"; tf.setText(panelDeLosNumeros);valorA = valorA + "9";}
+		else if(!simbolo.equalsIgnoreCase("ñ")){panelDeLosNumeros = panelDeLosNumeros + "9"; tf.setText(panelDeLosNumeros); valorB = valorB + "9";}}});
         
         Button ocho = new Button("8");
-        ocho.addActionListener(new ActionListener() {public void actionPerformed(ActionEvent e) {panelDeLosNumeros = panelDeLosNumeros + "8";tf.setText(panelDeLosNumeros);}});
+        ocho.addActionListener(new ActionListener() {public void actionPerformed(ActionEvent e) {if(simbolo.equalsIgnoreCase("ñ")) {panelDeLosNumeros = panelDeLosNumeros + "8"; tf.setText(panelDeLosNumeros);valorA = valorA + "8";}
+		else if(!simbolo.equalsIgnoreCase("ñ")){panelDeLosNumeros = panelDeLosNumeros + "8"; tf.setText(panelDeLosNumeros); valorB = valorB + "8";}}});
         
         Button siete = new Button("7");
-        siete.addActionListener(new ActionListener() {public void actionPerformed(ActionEvent e) {panelDeLosNumeros = panelDeLosNumeros + "7";tf.setText(panelDeLosNumeros);}});
+        siete.addActionListener(new ActionListener() {public void actionPerformed(ActionEvent e) {if(simbolo.equalsIgnoreCase("ñ")) {panelDeLosNumeros = panelDeLosNumeros + "7"; tf.setText(panelDeLosNumeros);valorA = valorA + "7";}
+		else if(!simbolo.equalsIgnoreCase("ñ")){panelDeLosNumeros = panelDeLosNumeros + "7"; tf.setText(panelDeLosNumeros); valorB = valorB + "7";}}});
         
         Button seis = new Button("6");
-        seis.addActionListener(new ActionListener() {public void actionPerformed(ActionEvent e) {panelDeLosNumeros = panelDeLosNumeros + "6";tf.setText(panelDeLosNumeros);}});
+        seis.addActionListener(new ActionListener() {public void actionPerformed(ActionEvent e) {if(simbolo.equalsIgnoreCase("ñ")) {panelDeLosNumeros = panelDeLosNumeros + "6"; tf.setText(panelDeLosNumeros);valorA = valorA + "6";}
+		else if(!simbolo.equalsIgnoreCase("ñ")){panelDeLosNumeros = panelDeLosNumeros + "6"; tf.setText(panelDeLosNumeros); valorB = valorB + "6";}}});
         
         Button cinco = new Button("5");
-        cinco.addActionListener(new ActionListener() {public void actionPerformed(ActionEvent e) {panelDeLosNumeros = panelDeLosNumeros + "5";tf.setText(panelDeLosNumeros);}});
+        cinco.addActionListener(new ActionListener() {public void actionPerformed(ActionEvent e) {if(simbolo.equalsIgnoreCase("ñ")) {panelDeLosNumeros = panelDeLosNumeros + "5"; tf.setText(panelDeLosNumeros);valorA = valorA + "5";}
+		else if(!simbolo.equalsIgnoreCase("ñ")){panelDeLosNumeros = panelDeLosNumeros + "5"; tf.setText(panelDeLosNumeros); valorB = valorB + "5";}}});
         
         Button cuatro = new Button("4");
-        cuatro.addActionListener(new ActionListener() {public void actionPerformed(ActionEvent e) {panelDeLosNumeros = panelDeLosNumeros + "4";tf.setText(panelDeLosNumeros);}});
+        cuatro.addActionListener(new ActionListener() {public void actionPerformed(ActionEvent e) {if(simbolo.equalsIgnoreCase("ñ")) {panelDeLosNumeros = panelDeLosNumeros + "4"; tf.setText(panelDeLosNumeros);valorA = valorA + "4";}
+		else if(!simbolo.equalsIgnoreCase("ñ")){panelDeLosNumeros = panelDeLosNumeros + "4"; tf.setText(panelDeLosNumeros); valorB = valorB + "4";}}});
         
         Button tres = new Button("3");
-        tres.addActionListener(new ActionListener() {public void actionPerformed(ActionEvent e) {panelDeLosNumeros = panelDeLosNumeros + "3";tf.setText(panelDeLosNumeros);}});
+        tres.addActionListener(new ActionListener() {public void actionPerformed(ActionEvent e) {if(simbolo.equalsIgnoreCase("ñ")) {panelDeLosNumeros = panelDeLosNumeros + "3"; tf.setText(panelDeLosNumeros);valorA = valorA + "3";}
+		else if(!simbolo.equalsIgnoreCase("ñ")){panelDeLosNumeros = panelDeLosNumeros + "3"; tf.setText(panelDeLosNumeros); valorB = valorB + "3";}}});
+        
         
         Button dos = new Button("2");
-        dos.addActionListener(new ActionListener() {public void actionPerformed(ActionEvent e) {panelDeLosNumeros = panelDeLosNumeros + "2";tf.setText(panelDeLosNumeros);}});
+        dos.addActionListener(new ActionListener() {public void actionPerformed(ActionEvent e) {if(simbolo.equalsIgnoreCase("ñ")) {panelDeLosNumeros = panelDeLosNumeros + "2"; tf.setText(panelDeLosNumeros);valorA = valorA + "2";}
+		else if(!simbolo.equalsIgnoreCase("ñ")){panelDeLosNumeros = panelDeLosNumeros + "2"; tf.setText(panelDeLosNumeros); valorB = valorB + "2";}}});
+        
         
         Button uno = new Button("1");
-        uno.addActionListener(new ActionListener() {public void actionPerformed(ActionEvent e) {panelDeLosNumeros = panelDeLosNumeros + "1";tf.setText(panelDeLosNumeros);}});
+        uno.addActionListener(new ActionListener() {public void actionPerformed(ActionEvent e) {if(simbolo.equalsIgnoreCase("ñ")) {panelDeLosNumeros = panelDeLosNumeros + "1"; tf.setText(panelDeLosNumeros);valorA = valorA + "1";}
+		else if(!simbolo.equalsIgnoreCase("ñ")){panelDeLosNumeros = panelDeLosNumeros + "1"; tf.setText(panelDeLosNumeros); valorB = valorB + "1";}}});
         
         Button cero = new Button("0");
-        cero.addActionListener(new ActionListener() {public void actionPerformed(ActionEvent e) {if(simbolo.equalsIgnoreCase("ñ")) {panelDeLosNumeros = panelDeLosNumeros + "0";tf.setText(panelDeLosNumeros);valorA = valorA + 0;}
-        																						else if(!simbolo.equalsIgnoreCase("ñ")){}}});
+        cero.addActionListener(new ActionListener() {public void actionPerformed(ActionEvent e) {if(simbolo.equalsIgnoreCase("ñ")) {panelDeLosNumeros = panelDeLosNumeros + "0"; tf.setText(panelDeLosNumeros);valorA = valorA + "0";}
+		else if(!simbolo.equalsIgnoreCase("ñ")){panelDeLosNumeros = panelDeLosNumeros + "0"; tf.setText(panelDeLosNumeros); valorB = valorB + "0";}}});
+        
         
         Button igual = new Button("=");
-        nueve.addActionListener(new ActionListener() {public void actionPerformed(ActionEvent e) {}});
+        igual.addActionListener(new ActionListener() {public void actionPerformed(ActionEvent e) {
+        	switch (simbolo) {
+			case "+": {float valorAntes = Float.parseFloat(valorA); float valorDespues = Float.parseFloat(valorB); float resultado = valorAntes + valorDespues; String resultadoStr = String.valueOf(resultado); valorA = resultadoStr; panelDeLosNumeros = resultadoStr; valorB = ""; tf.setText(panelDeLosNumeros); break;}
+			case "-": {float valorAntes = Float.parseFloat(valorA); float valorDespues = Float.parseFloat(valorB); float resultado = valorAntes - valorDespues; String resultadoStr = String.valueOf(resultado); valorA = resultadoStr; panelDeLosNumeros = resultadoStr; valorB = ""; tf.setText(panelDeLosNumeros); break;}
+			case "*": {float valorAntes = Float.parseFloat(valorA); float valorDespues = Float.parseFloat(valorB); float resultado = valorAntes * valorDespues; String resultadoStr = String.valueOf(resultado); valorA = resultadoStr; panelDeLosNumeros = resultadoStr; valorB = ""; tf.setText(panelDeLosNumeros); break;}
+			case "/": {float valorAntes = Float.parseFloat(valorA); float valorDespues = Float.parseFloat(valorB); float resultado = valorAntes / valorDespues; String resultadoStr = String.valueOf(resultado); valorA = resultadoStr; panelDeLosNumeros = resultadoStr; valorB = ""; tf.setText(panelDeLosNumeros); break;}
+        	}simbolo = "ñ";}});
         
         Button coma = new Button(",");
-        coma.addActionListener(new ActionListener() {public void actionPerformed(ActionEvent e) {System.out.print("9");}});
+        coma.addActionListener(new ActionListener() {public void actionPerformed(ActionEvent e) {if(simbolo.equalsIgnoreCase("ñ")) {panelDeLosNumeros = panelDeLosNumeros + "."; tf.setText(panelDeLosNumeros);valorA = valorA + ".";}
+		else if(!simbolo.equalsIgnoreCase("ñ")){panelDeLosNumeros = panelDeLosNumeros + "."; tf.setText(panelDeLosNumeros); valorB = valorB + ".";}}});
         
         centroPanel.add(siete);
         centroPanel.add(ocho);
@@ -107,7 +126,11 @@ public class CalculadoraClaseMain extends Frame {
         Panel derecharPanel = new Panel();
         derecharPanel.setLayout(new GridLayout(3, 1));
         Button ce = new Button("CE");
+        ce.addActionListener(new ActionListener() {	public void actionPerformed(ActionEvent e) {simbolo = "ñ"; panelDeLosNumeros = "0"; valorA = ""; valorB = ""; tf.setText("0");}});
+        
         Button c = new Button("C");
+        c.addActionListener(new ActionListener() {	public void actionPerformed(ActionEvent e) {tf.setText("0");}});
+        
         Button ne = new Button("NE");
         
         derecharPanel.add(ce);
