3cd1be6bc5d79118805e07ff10a68830fa165e71
Tomas-Falcon <tomasfalconblog@gmail.com>
2023-11-15 20:52:58 +0100

    actualizacion automatica

--- EX06 Diseno de Interfaz/src/Componentes/UsandoCheckBox.java
+++ EX06 Diseno de Interfaz/src/Componentes/UsandoCheckBox.java
@@ -4,8 +4,10 @@ import java.awt.Checkbox;
 import java.awt.FlowLayout;
 import java.awt.Frame;
 import java.awt.TextField;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
-public class UsandoCheckBox extends Frame{
+public class UsandoCheckBox extends Frame implements ItemListener{
 	
 	Checkbox cb1, cb2, cb3;
 	TextField etiqueta, etiqueta2;
@@ -29,12 +31,27 @@ public class UsandoCheckBox extends Frame{
 		
 		etiqueta.setEditable(false);
 		etiqueta2.setEnabled(false);
+		
 		add(etiqueta);
 		add(etiqueta2);
+				
+		cb1.addItemListener(e-> itemStateChanged(e));
+		cb2.addItemListener(e-> itemStateChanged(e));
+		cb3.addItemListener(this);
 		add(cb1);
 		add(cb2);
 		add(cb3);
 		
 		setVisible(true);
+	}
+
+	@Override
+	public void itemStateChanged(ItemEvent e) {
+		
+		if(e.getStateChange() == ItemEvent.SELECTED) {
+			System.out.println("Seleccionado");
+			etiqueta.setText("Seleccionado!");
+		}
+		
 	}
 }
