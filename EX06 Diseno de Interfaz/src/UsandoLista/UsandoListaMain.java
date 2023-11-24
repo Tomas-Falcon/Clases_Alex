5cf5cb1b111fa47f9eaf85756ccb1f5a3b2dfe2a
Tomas-Falcon <tomasfalconblog@gmail.com>
2023-11-24 19:52:47 +0100

    actualizacion automatica

--- EX06 Diseno de Interfaz/src/UsandoLista/UsandoListaMain.java
+++ EX06 Diseno de Interfaz/src/UsandoLista/UsandoListaMain.java
@@ -12,13 +12,13 @@ public class UsandoListaMain extends Frame{
 	UsandoListaMain(){
 		setSize(400,400);
 		String listaNombres [] = {"Anamaria", "Bruno", "Caro", "Diego", "Tomas", "Micaela", "Nina"};
-		lista = new List();
+		lista = new List(3);
 		lista.setMultipleMode(true);
 		
-		Panel panel = new Panel();
-		panel.setSize(400,250);
-		panel.setLayout(new BorderLayout());
-		panel.add(lista, BorderLayout.CENTER);
+		//Panel panel = new Panel();
+		//panel.setSize(400,250);
+		//panel.setLayout(new BorderLayout());
+		//panel.add(lista, BorderLayout.CENTER);
 		
 		
 		for(String a : listaNombres) {
