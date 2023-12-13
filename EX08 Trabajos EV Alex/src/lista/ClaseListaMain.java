d6994c837da396ba35a2a6ddd3487c736b530f14
Tomas-Falcon <tomasfalconblog@gmail.com>
2023-12-13 21:07:13 +0100

    actualizacion automatica

--- EX08 Trabajos EV Alex/src/lista/ClaseListaMain.java
+++ EX08 Trabajos EV Alex/src/lista/ClaseListaMain.java
@@ -1,21 +1,18 @@
 package lista;
 
-import java.awt.FlowLayout;
-
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JList;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.*;
+
+import java.awt.*;
+
 
 public class ClaseListaMain extends JFrame{
 
 	JTextField cajaNombre;
 	JLabel jLNombre; 
-	JList<String> listaA, listaB;
+	List listaA, listaB;
 	JButton botA, botB, botAñadir;
-	JPanel panelSuperior, panelInferiosUno,panelInferiosDos, panelInferiosTres, panel;
+	JPanel panelSuperior, panelInferiosUno,panelInferiosDos, panelInferiosTres;
 	
 	public static void main(String[] args) {
 		new ClaseListaMain();
@@ -23,25 +20,29 @@ public class ClaseListaMain extends JFrame{
 	
 	ClaseListaMain(){
 		super("Lista");
-		setSize(400,400);
+		setSize(350,200);
 		
-		panel = new JPanel();
+		
 		panelSuperior = new JPanel();
 		panelInferiosUno = new JPanel();
 		panelInferiosDos = new JPanel();
 		panelInferiosTres = new JPanel();
 		
-		listaA = new JList<String>();
-		listaB = new JList<String>();
+		listaA = new List();
+		listaB = new List();
+
 		
-		add(panel);
-		panel.add(panelSuperior);
 		
-		panelSuperior.setLayout(new FlowLayout());
+		add(panelSuperior,BorderLayout.NORTH);
+		
+		
 		
 		jLNombre = new JLabel("Nombre ");
 		cajaNombre = new JTextField("                        ");
 		botAñadir = new JButton("Añadir");
+		botA = new JButton(">>");
+		botB = new JButton("<<");
+
 		
 		panelSuperior.add(jLNombre);
 		panelSuperior.add(cajaNombre);
@@ -49,18 +50,22 @@ public class ClaseListaMain extends JFrame{
 		
 		
 		
-		panel.add(panelInferiosUno);
-		panel.add(panelInferiosDos);
-		panel.add(panelInferiosTres);
+		add(panelInferiosUno,BorderLayout.EAST);
+		add(panelInferiosDos,BorderLayout.CENTER);
+		add(panelInferiosTres,BorderLayout.WEST);
 		
 		panelInferiosUno.add(listaA);
 		panelInferiosDos.add(botA);
 		panelInferiosDos.add(botB);
 		panelInferiosTres.add(listaB);
 		
-		botAñadir.addActionListener(v-> {String nombre = cajaNombre.getText().trim(); listaA.add(nombre);});
-		botA.addActionListener(v-> {});
-		botB.addActionListener(v-> {});
+		botAñadir.addActionListener(v -> {
+		    String nombre = cajaNombre.getText().trim(); listaA.add(nombre);
+		});
+		botA.addActionListener(v-> {int indice = listaA.getSelectedIndex();	String seleccion = listaA.getItem(indice);
+		listaB.add(seleccion); listaA.remove(indice);});
+		botB.addActionListener(v-> {int indice = listaB.getSelectedIndex();	String seleccion = listaB.getItem(indice);
+		listaA.add(seleccion); listaB.remove(indice);});
 		
 		setVisible(true);
 	}
