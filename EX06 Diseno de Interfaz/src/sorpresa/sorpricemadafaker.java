c6bf3a428cf811f6381b8a4472c1f5844d6df07d
Tomas-Falcon <tomasfalconblog@gmail.com>
2023-12-15 19:25:24 +0100

    actualizacion automatica

---
+++ EX06 Diseno de Interfaz/src/sorpresa/sorpricemadafaker.java
@@ -0,0 +1,55 @@
+package sorpresa;
+
+
+import java.awt.*;
+
+
+public class sorpricemadafaker extends Frame{
+
+	public static void main(String[] args) {
+		new sorpricemadafaker();
+
+	}
+	
+	sorpricemadafaker() {
+		super("Menu item");
+		setSize(400,400);
+		
+		MenuItem m1 = new MenuItem("Item 1");
+		MenuItem m2 = new MenuItem("Item 2");
+		MenuItem m3 = new MenuItem("Item 3");
+		MenuItem m4 = new MenuItem("Item 4");
+		MenuItem m5 = new MenuItem("Item 5");
+		MenuItem m6 = new MenuItem("Item 6");
+		MenuItem m7 = new MenuItem("Salir");
+		
+		Menu menu = new Menu("Archivo");
+		
+		MenuItem mA = new MenuItem("Item A");
+		MenuItem mB = new MenuItem("Item B");
+		MenuItem mC = new MenuItem("Item C");
+		
+		Menu menu2 = new Menu("PERACIONES");
+		
+		menu.add(m1);
+		menu.add(m2);
+		menu.add(m3);
+		menu.add(m4);
+		menu.add(m5);
+		menu.add(m6);
+		menu.add(m7);
+		
+		menu2.add(mA);
+		menu2.add(mB);
+		menu2.add(mC);
+		
+		MenuBar menuBar = new MenuBar();
+		menuBar.add(menu);
+		menuBar.add(menu2);
+		
+		
+		setMenuBar(menuBar);
+		setVisible(true);
+	}
+
+}
