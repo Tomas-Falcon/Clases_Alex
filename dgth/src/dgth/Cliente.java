021e3193c5970e4dd6cf78775e6d2573dbfd1976
Tomas-Falcon <tomasfalconblog@gmail.com>
2024-01-15 19:46:23 +0100

    actualizacion del dia 15/01/2024 19:46:23,21

--- dgth/src/dgth/Cliente.java
+++ dgth/src/dgth/Cliente.java
@@ -9,33 +9,33 @@ import java.util.Scanner;
 
 public class Cliente {
 
-public static void main(String[] args) {
-Scanner sc = new Scanner(System.in);
-try {
-Socket cliente = new Socket("192.168.7.156", 8000);
-BufferedReader entrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
-PrintWriter salida = new PrintWriter(cliente.getOutputStream(), true);
-System.out.println(entrada.readLine());
-System.out.println(entrada.readLine());
-String nombre = sc.nextLine();
-salida.println(nombre);
-System.out.println(entrada.readLine());
-String mensaje = null;
-do {
-mensaje = sc.nextLine();
-salida.println(mensaje);
-}while(!mensaje.equalsIgnoreCase("Adios"));
-System.out.println(entrada.readLine());
-sc.close();
-cliente.close();
-entrada.close();
-salida.close();
-} catch (UnknownHostException e) {
-e.printStackTrace();
-} catch (IOException e) {
-e.printStackTrace();
-}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		try {
+			Socket cliente = new Socket("192.168.7.156", 8000);
+			BufferedReader entrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+			PrintWriter salida = new PrintWriter(cliente.getOutputStream(), true);
+			System.out.println(entrada.readLine());
+			System.out.println(entrada.readLine());
+			String nombre = sc.nextLine();
+			salida.println(nombre);
+			System.out.println(entrada.readLine());
+			String mensaje = null;
+			do {
+				mensaje = sc.nextLine();
+				salida.println(mensaje);
+			} while (!mensaje.equalsIgnoreCase("Adios"));
+			System.out.println(entrada.readLine());
+			sc.close();
+			cliente.close();
+			entrada.close();
+			salida.close();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-}
+	}
 
 }
