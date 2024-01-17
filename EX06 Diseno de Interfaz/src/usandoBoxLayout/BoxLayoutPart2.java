052adf811238b57cd87511fadcfc1674d638f521
Tomas-Falcon <tomasfalconblog@gmail.com>
2024-01-17 21:16:30 +0100

    actualizacion del dia 17/01/2024 21:16:30,00

---
+++ EX06 Diseno de Interfaz/src/usandoBoxLayout/BoxLayoutPart2.java
@@ -0,0 +1,66 @@
+package usandoBoxLayout;
+
+
+import java.awt.FlowLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class BoxLayoutPart2 {
+	
+	public static void main(String[] args) {
+		new BoxLayoutPart2();
+	}
+	
+	BoxLayoutPart2(){
+		JPanel panel1 = new JPanel();
+		JPanel panel2 = new JPanel();
+		JPanel panel3 = new JPanel();
+		
+		JButton jb1 = new JButton("1");
+		JButton jb2 = new JButton("2");
+		JButton jb3 = new JButton("3");
+		JButton jb4 = new JButton("4");
+		JButton jb5 = new JButton("5");
+		JButton jb6 = new JButton("6");
+		
+
+		JFrame frame =  new JFrame("Usando BoxLayout alineado");
+		frame.setSize(400,150);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new FlowLayout());
+		
+		
+		panel1.setBorder(BorderFactory.createTitledBorder("LEFT"));
+		panel2.setBorder(BorderFactory.createTitledBorder("CENTER"));
+		panel3.setBorder(BorderFactory.createTitledBorder("RIGHT"));
+		
+		//BoxLayout caja = new BoxLayout(panel1, BoxLayout.PAGE_AXIS);
+		//panel1.setLayout(caja);
+		
+		panel1.add(jb1);
+		panel1.add(jb2);
+		
+		
+		
+		//BoxLayout caja2 = new BoxLayout(panel2, BoxLayout.LINE_AXIS);
+		//panel2.setLayout(caja2);
+		
+		panel2.add(jb4);
+		panel2.add(jb3);	
+		
+		panel3.add(jb5);
+		panel3.add(jb6);
+
+		
+		frame.add(panel1);
+		frame.add(panel2);
+		frame.add(panel3);
+		
+		frame.setVisible(true);
+	}
+}
