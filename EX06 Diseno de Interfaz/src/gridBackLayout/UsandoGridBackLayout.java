c378f7ee02913f417835e20ae7ce6fb80a3711e4
Tomas-Falcon <tomasfalconblog@gmail.com>
2024-01-24 21:07:06 +0100

    actualizacion del dia 24/01/2024 21:07:06,43

--- EX06 Diseno de Interfaz/src/gridBackLayout/UsandoGridBackLayout.java
+++ EX06 Diseno de Interfaz/src/gridBackLayout/UsandoGridBackLayout.java
@@ -1,7 +1,50 @@
 package gridBackLayout;
 
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class UsandoGridBackLayout extends JFrame{
+public class UsandoGridBackLayout extends JFrame {
+    private static final long serialVersionUID = 1L;
 
+    public static void main(String[] args) {
+        new UsandoGridBackLayout();
+    }
+
+    UsandoGridBackLayout() {
+        super("Me he quedado muda de cantarte al oído");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500, 500);
+        JPanel panelPrincipal = new JPanel();
+
+        GridBagLayout layout = new GridBagLayout();
+        layout.rowWeights = new double[]{0.2, 0.8};
+        layout.columnWeights = new double[]{0.2, 0.2, 0.6};
+        panelPrincipal.setLayout(layout);
+
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        JButton boton1 = new JButton("Botón 1");
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        panelPrincipal.add(boton1, gbc);
+
+        JButton boton2 = new JButton("Botón 2");
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        panelPrincipal.add(boton2, gbc);
+
+        JButton boton3 = new JButton("Botón 3");
+        gbc.gridx = 2;
+        gbc.gridy = 2;
+        panelPrincipal.add(boton3, gbc);
+
+        add(panelPrincipal);
+        setVisible(true); 
+    }
 }
+
