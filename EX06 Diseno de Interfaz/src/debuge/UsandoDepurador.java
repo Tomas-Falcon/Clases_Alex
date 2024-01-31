9c8996f9f3addfefdea9c0c60de887caf3147f71
Tomas-Falcon <tomasfalconblog@gmail.com>
2024-01-31 20:55:24 +0100

    actualizacion del dia 31/01/2024 20:55:24,18

---
+++ EX06 Diseno de Interfaz/src/debuge/UsandoDepurador.java
@@ -0,0 +1,27 @@
+package debuge;
+
+public class UsandoDepurador {
+
+	   public static void main(String[] args) {
+	        paso1();
+	        paso2();
+	        paso3();
+	        paso4();
+	    }
+
+	    static void paso1() {
+	        System.out.println("Paso 1");
+	    }
+
+	    static void paso2() {
+	        System.out.println("Paso 2");
+	    }
+
+	    static void paso3() {
+	        System.out.println("Paso 3");
+	    }
+
+	    static void paso4() {
+	        System.out.println("Paso 4");
+	    }
+	}
