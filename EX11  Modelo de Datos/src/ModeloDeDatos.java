7e6df7933637849ee4cb2a99fd9ec6adac973af1
Tomas-Falcon <tomasfalconblog@gmail.com>
2024-02-12 20:03:56 +0100

    actualizacion del dia 12/02/2024 20:03:56,66

--- EX11  Modelo de Datos/src/ModeloDeDatos.java
+++ EX11  Modelo de Datos/src/ModeloDeDatos.java
@@ -1,39 +1,35 @@
-
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.MutableTreeNode;
 
-public class ModeloDeDatos extends DefaultTreeModel{
+public class ModeloDeDatos extends DefaultTreeModel {
 
-		private DefaultMutableTreeNode nodoRaiz;
-		
-		public ModeloDeDatos() {
-			super(new DefaultMutableTreeNode("root"));
-			nodoRaiz = (DefaultMutableTreeNode)getRoot();
-		}
-		
-		public void recargar(String unNodo) {
-			nodoRaiz.removeAllChildren();
-			int nodoAleatorio = (int) (Math.random()*10);
-			
-			for(int i = 0; i < nodoAleatorio; i++) {
-				DefaultMutableTreeNode nodohijo = new DefaultMutableTreeNode("un nodo hijo");
-				nodohijo.add(obtenerEstructura(unNodo.toString()));
-				nodoRaiz.add(nodohijo);
-				super.nodeStructureChanged(nodoRaiz);
-			}
-		}
-		
-		private MutableTreeNode obtenerEstructura(String string) {
-			DefaultMutableTreeNode retorno = new DefaultMutableTreeNode();
-			int nodoAleatorio = (int) (Math.random()*10);
-			for (int i = 0; i < nodoAleatorio; i++) {
-				DefaultMutableTreeNode nodoHijo = new DefaultMutableTreeNode();
-				retorno.add(nodoHijo);
-			}
-			return retorno;
-		}
-		
-	
+    private DefaultMutableTreeNode nodoRaiz;
 
+    public ModeloDeDatos() {
+        super(new DefaultMutableTreeNode("root"));
+        nodoRaiz = (DefaultMutableTreeNode) getRoot();
+    }
+
+    public void recargar(String unNodo) {
+        nodoRaiz.removeAllChildren();
+        int nodoAleatorio = (int) (Math.random() * 10);
+
+        for (int i = 0; i < nodoAleatorio; i++) {
+            DefaultMutableTreeNode nodoHijo = new DefaultMutableTreeNode("un nodo hijo");
+            nodoHijo.add(obtenerEstructura("algo")); // Modificado
+            nodoRaiz.add(nodoHijo);
+        }
+        super.nodeStructureChanged(nodoRaiz);
+    }
+
+    private MutableTreeNode obtenerEstructura(String contenido) {
+        DefaultMutableTreeNode retorno = new DefaultMutableTreeNode(contenido); // Modificado
+        int nodoAleatorio = (int) (Math.random() * 10);
+        for (int i = 0; i < nodoAleatorio; i++) {
+            DefaultMutableTreeNode nodoHijo = new DefaultMutableTreeNode("nodo hijo");
+            retorno.add(nodoHijo);
+        }
+        return retorno;
+    }
 }
